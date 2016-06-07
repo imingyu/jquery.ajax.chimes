aa8d53759af8746eda9459330872d3aeacfcafae
YuanMingyu <yuanmingyu@hipolearn.com>
2016-06-07 18:04:49 +0800

    core功能70%

--- src/chimes.core.js
+++ src/chimes.core.js
@@ -8,7 +8,9 @@
     }
 } (function ($, window, document, undefined) {
     'use strict';
-    
+
+    var arraySlice = Array.prototype.slice;
+
     /*
     ajax运行流程：
     1.初始化ajax配置参数；
@@ -21,56 +23,140 @@
      */
 
     function Chime(spec) {
+        this.id = spec.id || "CHIME" + ("" + Math.random()).replace('.', '');
         this.name = spec.name;
         this.spec = spec;
-        var __enabled = true;
+        this.__xhr = spec.xhr;
+        this.__ajaxOptions = spec.ajaxOptions;
+        this.__orgAjaxOptions = spec.orgAjaxOptions;
+        this.options = spec.options;
+
+        var __enabled = true,
+            installed=false,
+            uninstalled=false,
+            destroyed=false;
         this.isEnabled = function () {
             return __enabled;
         };
         this.enable = function () {
             __enabled = true;
             if (typeof spec.enable === "function") {
-                spec.enable.call(this);
+                spec.enable.apply(this, arraySlice.call(arguments));
             }
+            return this;
         };
         this.disable = function () {
             __enabled = false;
             if (typeof spec.disable === "function") {
-                spec.disable.call(this);
+                spec.disable.apply(this, arraySlice.call(arguments));
             }
+            return this;
         };
+        this.install= function () {
+            if (!this.isEnabled()) return false;
+            if(installed) return false;
+            var spec = this.spec;
+            installed=true;
+            return spec.install.apply(this, arraySlice.call(arguments));
+        },
+        this.uninstall= function () {
+            if (!this.isEnabled()) return false;
+            if(uninstalled) return false;
+            uninstalled=true;
+            this.__xhr = null;
+            this.__ajaxOptions = null;
+            this.__orgAjaxOptions = null;
+            delete this.__xhr;
+            delete this.__ajaxOptions;
+            delete this.__orgAjaxOptions;
+            var spec = this.spec;
+            return spec.uninstall.apply(this, arraySlice.call(arguments));
+        },
+        this.destroy= function () {
+            if(destroyed) return false;
+            destroyed=true;
+            var spec = this.spec;
+            spec.destroy.apply(this, arraySlice.call(arguments));
+            delete this.spec;
+            delete this.options;
+            delete this.__xhr;
+            delete this.__ajaxOptions;
+            delete this.__orgAjaxOptions;
+        }
     }
     Chime.prototype = {
         constructor: Chime,
-        install: function () {
-            var ops = this.spec;
-        },
-        uninstall: function () {
-        },
         run: function () {
-        },
-        destroy: function () {
+            if (!this.isEnabled()){
+                console.warn("风铃："+this.name+"已被禁用！");
+                return false;
+            }
+            if (uninstalled){
+                console.warn("风铃："+this.name+"已被卸载！");
+                return false;
+            }
+            if (destroyed){
+                console.warn("风铃："+this.name+"已被销毁！");
+                return false;
+            }
+            var spec = this.spec;
+            var result = spec.run.apply(this, arraySlice.call(arguments));
+            
+            //在xhr上执行完成的回调，执行本身的销毁事件
+            var self=this;
+            this.__xhr.always(function(){
+                self.destroy();
+                self = null;
+            });
+            return result;
         }
     };
 
 
     var AjaxChimers = {};
+    AjaxChimers.enabled = true;//是否启用风铃系统，说明此处可以集中控制，关闭后，设置的所有风铃将无效
     AjaxChimers.createChime = function (spec) {
-        var Constructor = function () {
+        var Constructor = function (chimeOptions, xhr, ajaxOptions, orgAjaxOptions) {
+            this.__xhr = this.__xhr || xhr;
+            this.__ajaxOptions = this.__ajaxOptions || ajaxOptions;
+            this.__orgAjaxOptions = this.__orgAjaxOptions || orgAjaxOptions;
+            this.options = this.options || $.extend(true, {}, Constructor.defaultOptions, chimeOptions);
         };
-        Constructor.prototype = new Chime();
+        
+        Constructor.instances = {};//已经实例化的风铃
+        Constructor.prototype = new Chime(spec);
         Constructor.prototype.constructor = Constructor;
-
-        if (Constructor.getDefaultOptions) {
-            Constructor.defaultOptions = Constructor.getDefaultOptions();
+        
+        Constructor.enabled=true;
+        Constructor.enable=function(){
+            Constructor.enabled=true;
+            for(var incId in Constructor.instances){
+                var inc = Constructor.instances[incId];
+                if(!inc.isEnabled()){
+                    inc.enable();
+                }
+            }
+        };
+        Constructor.disable=function(){
+            Constructor.enabled=false;
+            for(var incId in Constructor.instances){
+                var inc = Constructor.instances[incId];
+                if(inc.isEnabled()){
+                    inc.disable();
+                }
+            }
+        };
+        if (spec.getDefaultOptions) {
+            Constructor.defaultOptions = spec.getDefaultOptions();
         }
         AjaxChimers[spec.name] = Constructor;
+        return Constructor;
     };
-    AjaxChimers.destroyChime = function (chimeName) {
+    AjaxChimers.destroyChime = function (chimeName, chimeId) {
         var chime = AjaxChimers[chimeName];
-        if (chime && chime.destroy) {
-            chime.destroy();
-            delete AjaxChimers[chimeName];
+        if (chime && chime.instances[chimeId]) {
+            chime.instances[chimeId].destroy();
+            delete chime.instances[chimeId];
         }
     };
     AjaxChimers.enableChime = function (chimeName) {
@@ -85,5 +171,66 @@
             chime.disable();
         }
     };
+    AjaxChimers.setChimeDefaultOptions = function (chimeName, options) {
+        var chime = AjaxChimers[chimeName];
+        chime.defaultOptions = chime.defaultOptions || {};
+        $.extend(true, chime.defaultOptions, options);
+    };
     $.AjaxChimers = AjaxChimers;
+
+    //风铃的安装与运行
+    $.ajaxPrefilter(function (ops, orgOps, jqXhr) {
+        //如果存在配置风铃，并且是启用了风铃系统
+        if (ops.chimers && $.AjaxChimers.enabled !== false && ops.chimers.enabled !== false) {
+            var beforeSendHandles = [],//定义一个beforeSend队列，保存所有的beforeSend事件
+                ajaxBeforeSend = ops.beforeSend,//如果当前ajax请求配置了此设置（占据第一项beforeSend）
+                ajaxChimers = {};//已经在此ajax上的风铃实例
+
+            ops.chimeInstances = jqXhr.chimeInstances = ajaxChimers;//将风铃实例集合放置在ajax对象上
+            ops.beforeSend = null;
+
+            for (var chimeName in ops.chimers) {
+                var chime = $.AjaxChimers[chimeName],
+                    chimeOptions = ops.chimers[chimeName];
+                if (chime) {
+                    var chimeInc = new chime(chimeOptions, jqXhr, ops, orgOps);
+                    chimeInc.install();
+                    chimeInc.run();
+
+                    //将风铃的beforeSend事件插入到队列中
+                    if (typeof ops.beforeSend === "function") {
+                        beforeSendHandles.push(ops.beforeSend);
+                        ops.beforeSend = null;
+                    }
+                    //缓存实例
+                    ajaxChimers[chimeName] = chimeInc;
+                    chime.instances[chimeInc.id] = chimeInc;
+                } else {
+                    console.warn(pid + "挂件不存在或不可用");
+                }
+            }
+
+            //将beforeSend队列放入真正的beforeSend函数中
+            var beforeSendCallBacks = $.Callbacks("once memory unique");
+            beforeSendCallBacks.add(beforeSendHandles);
+            ops.beforeSend = function (jqXhr, ops) {
+                if (typeof ajaxBeforeSend === "function") {
+                    //获取occupyFirstBeforeSend设置项，这项配置觉得ajax本身的beforeSend事件在何处（所有风铃之后或之前）执行，这有利于在beforeSendh中禁用某些风铃
+                    if (ops.chimers.occupyFirstBeforeSend === true) {
+                        var result = ajaxBeforeSend.apply(ops, [jqXhr, ops]);
+                        if (result != false) {
+                            beforeSendCallBacks.fireWith(ops, [jqXhr, ops]);
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        beforeSendCallBacks.fireWith(ops, [jqXhr, ops]);
+                        return ajaxBeforeSend.apply(ops, [jqXhr, ops]);
+                    }
+                } else {
+                    beforeSendCallBacks.fireWith(ops, [jqXhr, ops]);
+                }
+            };
+        }
+    });
 }));
